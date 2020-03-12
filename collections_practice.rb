5f26d10562a3709a2eedfaaf2933fe4f65e0f620
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-12 18:18:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
 
+def sort_array_asc(num)
+  num.sort
+end
+
+def sort_array_desc(num)
+  num.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(words)
+  words.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |element| 
+      element[2] = '$'
+    end
+    array
+end
+
+def find_a(array)
+  array.select do |element|
+    element[0] == 'a' 
+  end
+end
+
+def sum_array(array)
+ array_to_sum = array.select do |num|
+                (2..(num - 1)).each do |n|
+                 return false if num % n == 0
+                 end
+                 true
+                 end
+                end 
+ (array_to_sum[0]..array_to_sum[-1]).inject {|sum,n| sum + n}
+end
+
+
