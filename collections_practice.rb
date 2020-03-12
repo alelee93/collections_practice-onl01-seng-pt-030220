bd6bb7a2be38770848700da0920f06e9052a985a
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-12 18:50:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,7 +51,11 @@ def sum_array(array)
  def add_s(array)
    new_array = []
    array.each_with_index do |a,index|
-     new = a + 's' if index != 1 else a
+     if index != 1
+     new = a + 's'  
+   else new = a
+   end 
+   
      new_array.push(new)
      
    end
